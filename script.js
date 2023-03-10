9ab6309148d46b54cd747ded688754a2020eaad4
Dathasai <dathasaichandu@gmail.com>
2023-03-10 21:00:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// const typingParagraph = document.querySelector('.typing-paragraph');
+// const section = document.querySelector('.typing-section');
+// const windowHeight = window.innerHeight;
+
+// function animateParagraph() {
+//     const sectionTop = section.getBoundingClientRect().top;
+//     const sectionBottom = section.getBoundingClientRect().bottom;
+
+//     // Check if the section is in view
+//     if (sectionTop <= windowHeight / 2 && sectionBottom >= windowHeight / 2) {
+//         typingParagraph.classList.add('typing');
+//     } else {
+//         typingParagraph.classList.remove('typing');
+//     }
+// }
+
+// window.addEventListener('scroll', animateParagraph);
